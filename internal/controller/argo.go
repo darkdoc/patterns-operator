9d0947566a05b4d877fa3f1acd86634542ffd459
Akos Eros <aeros@redhat.com>
2024-11-04 15:08:20 +0100

    Add application health assessment to argocd app
    This was removed in argocd 1.8 see
    https://github.com/argoproj/argo-cd/issues/3781
    To make application ordering, and dependencies possible, this is needed
    for the app of apps pattern.

--- internal/controller/argo.go
+++ internal/controller/argo.go
@@ -240,6 +240,24 @@ g, admin, role:admin`
 				VolumeMounts:   initVolumeMounts,
 				Volumes:        initVolumes,
 			},
+			ResourceHealthChecks: []argooperator.ResourceHealthCheck{
+				{
+					Kind: "Application",
+					Check: `
+						hs = {}
+      					hs.status = "Progressing"
+      					hs.message = ""
+						if obj.status ~= nil then
+							if obj.status.health ~= nil then
+							hs.status = obj.status.health.status
+							if obj.status.health.message ~= nil then
+								hs.message = obj.status.health.message
+							end
+							end
+						end
+						return hs`,
+				},
+			},
 			ResourceExclusions: `- apiGroups:
   - tekton.dev
   clusters:
