9073bda7120e61678c8331fbcf33a52056b316ce
Michele Baldessari <michele@acksyn.org>
2026-03-02 18:09:09 +0100

    make catalog configurable

--- internal/controller/defaults.go
+++ internal/controller/defaults.go
@@ -79,6 +79,12 @@ const (
 	GiteaDefaultPasswordLen = 15
 )
 
+// Pattern Catalog
+const (
+	// PatternCatalogDefaultImage is empty, meaning the image deployed by kustomize/OLM is used
+	PatternCatalogDefaultImage = ""
+)
+
 // Experimental Capabilities that can be enabled
 // Currently none
 
@@ -92,6 +98,7 @@ var DefaultPatternOperatorConfig = map[string]string{
 	"gitea.helmRepoUrl":          GiteaHelmRepoUrl,
 	"gitea.chartVersion":         GiteaDefaultChartVersion,
 	"analytics.enabled":          "true",
+	"catalog.image":              PatternCatalogDefaultImage,
 }
 
 type GitOpsConfig map[string]string
